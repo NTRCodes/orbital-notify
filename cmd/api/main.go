63928e8417e93d06262d63b468b4b38821330a62
nr44c <nigel.ross85@gmail.com>
2026-03-30 17:39:11 -0700

    configure http.Sever with timeouts an and load addr from config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/ntrcodes/orbital-notify/internal/config"
 )
 
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := &http.Server{
+		Addr:         ":" + cfg.Port,
+		Handler:      nil,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "ok")
 	})
 
-	fmt.Println("API running on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+	fmt.Println("API running on :" + cfg.Port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
 	}
 }
